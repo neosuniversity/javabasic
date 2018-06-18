bd0dae8be4cae4c7d83a48925673856357a93f1d
hector77 <hector.hidalgo@globant.com>
2018-06-18 12:09:48 -0500

    add new code about calculadora JavaFx

---
+++ src/org/neosuniversity/unidad3/calculadora/Calculadora.java
@@ -0,0 +1,112 @@
+package org.neosuniversity.unidad3.calculadora;
+
+/**
+ * Created by camus on 05/06/2018.
+ */
+public class Calculadora {
+
+
+    private double operadorA;
+    private double operadorB;
+    private String operacion;
+    private double resultado;
+
+    public Calculadora() {}
+
+    public Calculadora(double operadorA, double operadorB) {
+        this.operadorA = operadorA;
+        this.operadorB = operadorB;
+    }
+
+    public Calculadora(double operadorA, double operadorB,String operacion) {
+        this.operadorA = operadorA;
+        this.operadorB = operadorB;
+        this.operacion = operacion;
+    }
+
+    public double getOperadorA() {
+        return operadorA;
+    }
+
+    public void setOperadorA(double operadorA) {
+        this.operadorA = operadorA;
+    }
+
+    public double getOperadorB() {
+        return operadorB;
+    }
+
+    public void setOperacorB(double operadorB) {
+        this.operadorB = operadorB;
+    }
+
+    public String getOperacion() {
+        return operacion;
+    }
+
+    public void setOperacion(String operacion) {
+        this.operacion = operacion;
+    }
+
+    public double getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(double resultado) {
+        this.resultado = resultado;
+    }
+
+    private  double suma(Calculadora calculadora){
+        double opeardorA = calculadora.getOperadorA();
+        double opeardorB = calculadora.getOperadorB();
+
+        return (opeardorA + opeardorB);
+    }
+
+    private  double resta(Calculadora calculadora){
+        double opeardorA = calculadora.getOperadorA();
+        double opeardorB =calculadora.getOperadorB();
+
+        return (opeardorA - opeardorB);
+    }
+
+    private  double multiplicacion(Calculadora calculadora){
+        double opeardorA = (calculadora.getOperadorA());
+        double opeardorB = (calculadora.getOperadorB());
+
+        return (opeardorA * opeardorB);
+    }
+
+    private  double divison(Calculadora calculadora){
+        double opeardorA = (calculadora.getOperadorA());
+        double opeardorB = (calculadora.getOperadorB());
+
+        return (opeardorA / opeardorB);
+    }
+
+    public void executeOperation(Calculadora calculadora){
+        switch (calculadora.getOperacion()){
+            case "+":{
+                calculadora.setResultado(suma(calculadora));
+                break;
+            }
+            case "-":{
+                calculadora.setResultado(resta(calculadora));
+                break;
+            }
+            case "*":{
+                calculadora.setResultado(multiplicacion(calculadora));
+                break;
+            }
+            case "/":{
+                calculadora.setResultado(divison(calculadora));
+                break;
+            }
+            default:{
+                // calculadora.setResultado("ERROR");
+                break;
+            }
+        }
+    }
+
+}
---
+++ src/org/neosuniversity/unidad3/calculadora/CalculadoraController.java
@@ -0,0 +1,85 @@
+package org.neosuniversity.unidad3.calculadora;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+
+public class CalculadoraController {
+
+    @FXML
+    private TextField txtOperadorA;
+
+    @FXML
+    private TextField txtOperadorB;
+
+    @FXML
+    private TextField txtResultado;
+
+    @FXML
+    private TextArea txtOperacion;
+
+    private Calculadora calculadora;
+
+
+    public void actionSuma(ActionEvent event){
+        ejectutaOperacion("+",Alert.AlertType.INFORMATION);
+    }
+
+    public void actionResta(ActionEvent event){
+        ejectutaOperacion("-",Alert.AlertType.ERROR);
+    }
+
+    public void actionMultiplicacion(ActionEvent event){
+        ejectutaOperacion("*",Alert.AlertType.WARNING);
+    }
+
+    public void actionDivision(ActionEvent event){
+        ejectutaOperacion("/",Alert.AlertType.CONFIRMATION);
+    }
+
+    public void actionlimpiar(ActionEvent event){
+        txtOperadorA.clear();
+        txtOperadorB.clear();
+        txtResultado.clear();
+        txtOperacion.clear();
+    }
+
+    private void ejectutaOperacion(String operador,Alert.AlertType value){
+        if(validaOperadores(txtOperadorA.getText(), txtOperadorB.getText())){
+            enviaMensajeError(value);
+        }else {
+            System.out.println("valor" + txtOperadorA.getText() + operador + txtOperadorB.getText());
+            txtOperacion.setText(txtOperadorA.getText() + operador + txtOperadorB.getText());
+            calculadora = ejecutaOparacion(txtOperadorA.getText(), txtOperadorB.getText(), operador);
+
+            txtResultado.setText(String.valueOf(calculadora.getResultado()));
+        }
+    }
+
+    private Calculadora ejecutaOparacion(String operadorA, String operadorB,String operador){
+        Calculadora calculadora = new Calculadora(Double.valueOf(operadorA),Double.valueOf(operadorB),operador);
+        calculadora.executeOperation(calculadora);
+        return calculadora;
+    }
+
+    private boolean validaOperadores(String operadorA, String OperadorB){
+        boolean bandera=false;
+        if(operadorA!=null && OperadorB!=null){
+            if(operadorA.isEmpty()|| OperadorB.isEmpty()){
+                bandera=true;
+            }
+        }
+        return bandera;
+    }
+
+    private void enviaMensajeError(Alert.AlertType value){
+        Alert alerta = new Alert(value);
+        alerta.setTitle("Error en operandos");
+        alerta.setHeaderText("***Favor de verificar los operadores ***");
+        alerta.show();
+    }
+
+}
